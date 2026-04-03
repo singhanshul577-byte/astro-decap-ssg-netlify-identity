15feb7465cdf941e15fa55da3bc63b817c9e7e49
singhanshul577-byte <singhanshul577@gmail.com>
2026-04-04 00:28:30 +0530

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -25,3 +25,30 @@ const config: AstroUserConfig = defineConfig({
 });
 
 export default defineConfig(config);
+
+import { defineConfig } from "astro/config";
+import type { AstroUserConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+const config: AstroUserConfig = defineConfig({
+  site: "http://www.example.com",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
+  // YE WALA HISSA ZAROOR DAALEIN
+  vite: {
+    resolve: {
+      alias: {
+        'react-dom/client': 'react-dom/client',
+        'react': 'react',
+        'react-dom': 'react-dom'
+      },
+    },
+  },
+});
+
+export default defineConfig(config);
